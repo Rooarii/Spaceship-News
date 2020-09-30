5b9636d10fd6d00d06b6d848198296099635588a
Rooarii MANUEL <rooarii.manuel@gmail.com>
2020-09-30 06:20:03 +0200

    style* root color defined, style added on main tag, icons added; js- content objects adedd

--- script.js
+++ script.js
@@ -8,24 +8,16 @@ burger.addEventListener('click', () => {
 
 
 // cards in main
-// content
-const content = [
-    {
-      name: "Title 1",
-      picture: "./images/article1.jpeg"
-    },
-    {
-      name: "Title 2",
-      picture: "./images/article2.jpeg"
-    },
-    {
-      name: "Title 3",
-      picture: "./images/article3.jpeg"
-    }
-]
 
 const wrap = document.querySelector(".wrap");
-
+/** card structure
+/*  article
+/*      card
+/*          img
+/*          card-body
+/*              card-title
+/*              card-button
+*/
 function createCard(title, picture) {
     const article = document.createElement("article");
     article.classList.add("article");
@@ -60,3 +52,60 @@ function loadCards (){
       }      
 }
 window.onload = loadCards;
+
+
+// content
+const content = [
+    {
+      name: "About that space illustration you keep seeing around",
+      picture: "./images/article1.jpeg"
+    },
+    {
+      name: "Why ‘Contact’ Is the Most Important Space Movie Ever Made",
+      picture: "./images/article2.jpeg"
+    },
+    {
+      name: "My Journey to the Edge of Space",
+      picture: "./images/article3.jpeg"
+    },
+    {
+        name: "Has NASA’s Kepler Mission Discovered an Alien Megastructure?",
+        picture: "./images/article4.jpeg"
+    },
+    {
+        name: "Is Humanity About To Accidentally Declare Interstellar War On Alien Civilizations?",
+        picture: "./images/article5.jpeg"
+    },
+    {
+        name: "Why Mars Matters",
+        picture: "./images/article6.jpeg"
+    },
+    {
+        name: "Elon Musk isn’t an alien. He’s becoming a robot instead.",
+        picture: "./images/article7.jpeg"
+    },
+    {
+        name:"Jabba the Trump: A comic and coloring page!",
+        picture: "./images/article8.jpeg"
+    },
+    {
+        name:"Movie Review: Ridley Scott returns to the franchise he helped to spawn in “Alien: Covenant”.",
+        picture: "./images/article9.jpeg"
+    },
+    {
+        name: "Airbus is building 900 satellites to beam internet from space",
+        picture: ""
+    },
+    {
+        name: "Why NASA’s EmDrive Might Be the Most Exciting Breakthrough in Tech Right Now",
+        picture: "./images/article10.jpeg"
+    },
+    {
+        name: "Black Holes Must Have Singularities, Says Einstein’s Relativity",
+        picture:"./images/article11.jpeg"
+    },
+    {
+        name: "Spock and The Collective Consciousness",
+        picture: "./images/article12.png"
+    }
+]
